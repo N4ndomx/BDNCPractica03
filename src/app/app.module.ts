d38c9bafe0682556f9b5ace87e71e80d0168218a
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-05-11 03:13:35 -0600

    add personas component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasCrudComponent } from './personas-crud/personas-crud.component';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from './personas-crud/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasCrudComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
